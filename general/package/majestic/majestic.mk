8d5eb4d023b790c62181c1442241fa8b309e97e4
xulang <xu_lang@foxmail.com>
2026-07-25 21:12:17 +0800

    majestic: use fpv default configuration

--- general/package/majestic/majestic.mk
+++ general/package/majestic/majestic.mk
@@ -19,9 +19,14 @@ MAJESTIC_DEPENDENCIES += \
 	opus-openipc \
 	json-c
 
+define MAJESTIC_PATCH_EXPOSURE_MIN
+	perl -0777 -pi -e 's/\x05\x28\x4f\xf4\x7a\x73\xb8\xbf\x05\x20\xc8\x28/\x01\x28\x4f\xf4\x7a\x73\xb8\xbf\x01\x20\xc8\x28/ or die "majestic exposure clamp pattern not found\n"' $(@D)/majestic
+endef
+MAJESTIC_POST_EXTRACT_HOOKS += MAJESTIC_PATCH_EXPOSURE_MIN
+
 define MAJESTIC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/majestic.yaml
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(MAJESTIC_PKGDIR)/files/majestic.yaml
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(MAJESTIC_PKGDIR)/files/S95majestic
